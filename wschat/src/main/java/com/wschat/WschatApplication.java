eeadffb897246a1837b917339f6770b53b03c1dd
xiebanglin <2419507265@qq.com>
2020-12-01 15:44:15 +0800

    新增springboot作为服务端html客户端

---
+++ wschat/src/main/java/com/wschat/WschatApplication.java
@@ -0,0 +1,25 @@
+package com.wschat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@SpringBootApplication
+@EnableWebSocket
+public class WschatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WschatApplication.class, args);
+    }
+    /**
+     * 初始化Bean，它会自动注册使用了 @ServerEndpoint 注解声明的 WebSocket endpoint
+     *
+     * @return
+     */
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
